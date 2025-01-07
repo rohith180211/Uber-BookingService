ef1a0ef4311fad5a0a92465d1fe9e6d58dbaf4f0
Rohith Janwadkar <rohithjanwadkar@Rohiths-MacBook-Air.local>
2025-01-06 18:44:35 -0600

    Made a few changes

--- src/main/java/org/example/uberbookingservice/services/BookingServiceImpl.java
+++ src/main/java/org/example/uberbookingservice/services/BookingServiceImpl.java
@@ -15,48 +15,61 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 
 @Service
 public class BookingServiceImpl implements BookingService {
+
     private final PassengerRepository passengerRepository;
     private final BookingRepository bookingRepository;
+
     private final RestTemplate restTemplate;
+
+
+
+
     private static final String LOCATION_SERVICE = "http://localhost:7777";
 
-    public BookingServiceImpl(PassengerRepository passengerRepository, BookingRepository bookingRepository) {
+    public BookingServiceImpl(PassengerRepository passengerRepository,
+                              BookingRepository bookingRepository) {
         this.passengerRepository = passengerRepository;
         this.bookingRepository = bookingRepository;
         this.restTemplate = new RestTemplate();
+
     }
 
+
+    @Override
     public createBookingResponseDto createBooking(createBookingDto bookingDetails) {
-        Optional<Passenger> passenger=passengerRepository.findById(bookingDetails.getPassengerId());
+        Optional<Passenger> passenger = passengerRepository.findById(bookingDetails.getPassengerId());
         Booking booking = Booking.builder()
+
                 .bookingStatus(BookingStatus.ASSIGNING_DRIVER)
                 .startLocation(bookingDetails.getStartLocation())
-                .endLocation(bookingDetails.getEndLocation())
+               .endLocation(bookingDetails.getEndLocation())
                 .passenger(passenger.get())
                 .build();
-        Booking newBooking=bookingRepository.save(booking);
+        Booking newBooking = bookingRepository.save(booking);
 
-        getNearbyDriversRequestDto  requestDto= getNearbyDriversRequestDto
-                .builder()
+        // make an api call to location service to fetch nearby drivers
+
+        getNearbyDriversRequestDto request = getNearbyDriversRequestDto.builder()
                 .latitude(bookingDetails.getStartLocation().getLatitude())
                 .longitude(bookingDetails.getStartLocation().getLongitude())
                 .build();
-        // make an api call to location service to fetch nearby drivers
-        ResponseEntity<DriverLocationDto[]> res=restTemplate.postForEntity(LOCATION_SERVICE+"/api/location/nearbydrivers",requestDto,DriverLocationDto[] .class);
-        if(res.getStatusCode().is2xxSuccessful() && res.getBody()!=null){
-            List<DriverLocationDto> driverLocations= Arrays.asList(Objects.requireNonNull(res.getBody()));
-            driverLocations.forEach(
-                    driverLocation -> {System.out.println(driverLocation.getDriverId()+" "+"lat: "+driverLocation.getLatitude()+" long: "+driverLocation.getLongitude());}
-            );
+
+
+        ResponseEntity<DriverLocationDto[]> result = restTemplate.postForEntity(LOCATION_SERVICE + "/api/location/nearby/drivers", request, DriverLocationDto[].class);
+
+        if(result.getStatusCode().is2xxSuccessful() && result.getBody() != null) {
+            List<DriverLocationDto> driverLocations = Arrays.asList(result.getBody());
+            driverLocations.forEach(driverLocationDto -> {
+                System.out.println(driverLocationDto.getDriverId() + " " + "lat: " + driverLocationDto.getLatitude() + "long: " + driverLocationDto.getLongitude());
+            });
         }
-        return createBookingResponseDto
-                .builder()
+
+        return createBookingResponseDto.builder()
                 .bookingId(newBooking.getId())
                 .bookingStatus(newBooking.getBookingStatus().toString())
                 .build();
